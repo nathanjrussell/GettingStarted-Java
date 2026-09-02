d8f7968a23044012054e341a0b1cdfad060d4e6f
Nathan Russell <njruss01@louisville.edu>
2026-09-02 09:52:11 -0400

    demonstrated encapsulation with Person class

--- SyntaxDemo/src/main/java/io/github/nathanjrussell/Main.java
+++ SyntaxDemo/src/main/java/io/github/nathanjrussell/Main.java
@@ -1,52 +1,26 @@
 package io.github.nathanjrussell;
 
-import java.util.ArrayList;
-
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
 
+        Person nathan = new Person(39,"Nathan","Russell");
+        System.out.println(nathan.getAge());
 
 
-        byte d = 127;
-        byte neg_d = -128;
-        short b = 32767;
-        short neg_b = -32768;
-        int a = 2147483647;
-        int neg_a = -2147483648;
-        long e =2147483648L;
+        Person[] peopleArray = new Person[5];
+        peopleArray[0] = nathan;
+        peopleArray[1] = new Person(20,"Jiwoo","Hang");
+        peopleArray[2] = new Person(19,"Marissa","McCubbin");
+        peopleArray[3] = new Person(21,"Kamil","Zavala");
+        peopleArray[4] = new Person(28,"Keaston","Edwards");
 
-        double dd = 3.1415;
-        float f = 3.14f;
-        boolean bool = true;
-        char ch = 'a';
-
-        int[] int_array = {1,2,3,5,6,7};
-
-        for (int i = 0; i < int_array.length; i++) {
-            System.out.println(int_array[i]);
-        }
-
-        for (int i : int_array) {
-            System.out.println(i);
-        }
-
-        ArrayList<Integer> dyn_list = new ArrayList<>();
-        dyn_list.add(1);
-        dyn_list.add(2);
-        dyn_list.add(7);
-        dyn_list.add(42);
-        dyn_list.add(65);
-        dyn_list.add(67);
-
-        for(int i : dyn_list) {
-            System.out.println(i);
-        }
-
-        for (Integer integer : dyn_list) {
-            System.out.println(integer);
+        for (Person person : peopleArray) {
+            System.out.println("Here is a new person shown below!");
+            System.out.println("\t" + person.getFirstName() + " " + person.getLastName());
+            System.out.println("\tAge: " + person.getAge());
         }
 
     }
